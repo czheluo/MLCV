ec77a54ab9522b2a06e7a0236da5d350326908f6
Chenzhe Luo <czheluo@gmail.com>
2018-04-20 16:09:40 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -426,7 +426,7 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 }*/
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -455,4 +455,30 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   
   setTimeout(showSlides, 8000); // Change image every 2 seconds
+}*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
