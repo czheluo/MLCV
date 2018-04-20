f993e2601296664f358949dbfb281ab4274da837
Chenzhe Luo <czheluo@gmail.com>
2018-04-20 15:50:38 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -454,5 +454,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
 }
