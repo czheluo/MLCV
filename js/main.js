128b9ee42713be278fdf2310a01efc1118b1c564
Chenzhe Luo <czheluo@gmail.com>
2018-04-30 17:08:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -515,10 +515,10 @@ function showSlides(n) {
   })
 
 })*/
-var links = document.links;
+/*var links = document.links;
 
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
    if (links[i].hostname != window.location.hostname) {
        links[i].target = '_blank';
    } 
-}
+}*/
